1bb8e9952458a8b7c60c85fa56d1098a1e570f07
abraaoribeiro <abraaohdg@gmail.com>
2021-07-02 20:49:08 -0300

    primeiro commit

---
+++ src/main/java/swapi/springboot/lab/SwapiSpringbootLabApplication.java
@@ -0,0 +1,13 @@
+package swapi.springboot.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiSpringbootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiSpringbootLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swapi/springboot/lab/SwapiSpringbootLabApplicationTests.java
@@ -0,0 +1,13 @@
+package swapi.springboot.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiSpringbootLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
